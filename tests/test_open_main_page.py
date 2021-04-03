9d708e7d4d457e31f3ca5e6eab27ec5b073e36fb
uraniumcore238@gmail.com <Qwe2asd9>
2021-04-03 21:27:43 +0500

    added allure annotations

--- tests/test_open_main_page.py
+++ tests/test_open_main_page.py
@@ -1,11 +1,17 @@
+import allure
+
 from page_objects.MainPageObject import MainPageObject
 from utilities.BaseClass import BaseClass
 
 
 class TestOpenMainPage(BaseClass):
 
+    @allure.feature('Аллюр фича')
+    @allure.story('Аллюр стори')
+    @allure.severity('critical')
     def test_assert_the_main_page(self):
 
-        mainPageObject = MainPageObject(self.driver)
-        mainPageObject.assert_the_header()
+        with allure.step('Open main page'):
+            mainPageObject = MainPageObject(self.driver)
+            mainPageObject.assert_the_header()
 
